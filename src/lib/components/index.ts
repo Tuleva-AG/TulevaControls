00801cde2683beaf9028b70b0caa8f466a1ac869
Jaisree ramesh <jaisree.ramesh@tuleva.de>
2022-11-28 15:15:45 +0100

    action updated

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,6 @@
-export { default as HoursMetaData } from "./TulevaMetaData";
+export { default as TulevaMetaData } from "./TulevaMetaData";
 export { default as HeaderColumn } from "./TulevaHeaderColumn";
-export { default as HoursEditor } from "./TulevaEditor";
-export { default as HoursPage } from "./TulevaPage";
+export { default as TulevaEditor } from "./TulevaEditor";
+export { default as TulevaPage } from "./TulevaPage";
 export { default as PageHeader } from "./TulevaPageHeader";
 export * from "./tulevaSharedContext";
